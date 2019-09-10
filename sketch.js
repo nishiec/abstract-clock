8f34399e4a7d9045ee6f7f17799d194ebee99dd5
nishiec <nishat.tc3@gmail.com>
2019-09-10 00:08:10 -0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -5,16 +5,23 @@ function setup() {
 
 function draw() { 
   background(0); //black background
-  
-  //seconds 
-  noStroke();
-  fill(255);
-  ellipse(400,400,300,300);
-  fill(255,209,220);
-  arc(400,400,300,300,1.5*PI,(second()/60*PI));
+  seconds(); //tiny circular abstract clock 
 
   
 
 }
 
+function seconds(){
+  //seconds positioned in the bottom right corner
+  noStroke();
+  fill(255);
+  ellipse(600,600,300,300);
+  fill(255,209,220);
+  //in radians starting place is 0pi
+  arc(600,600,300,300,0,(second()/30*PI));
+}
+
+function minutes(){
   
+  
+}
