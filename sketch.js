f73c159066354aacfc987ac9c790d4ad21270f4e
nishiec <nishat.tc3@gmail.com>
2019-09-10 01:37:47 -0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,26 +1,22 @@
 w = 800;
 h = 800;
 function setup() { 
-
-  createCanvas(h, h);
-  
+  createCanvas(w, h);
 } 
 
 function draw() { 
-  background(0); //black background
-  minutes();
-  seconds(); //tiny circular abstract clock 
-
-  
-
+  background(201, 233, 246); //sky background
+  minutes(); // the sky turning into night
+  seconds(); // circular abstract sun or moon
+  hours(); // clouds
 }
 
 function seconds(){
   //seconds positioned in the bottom right corner
   noStroke();
-  fill(255);
+  fill(245, 243, 206);
   ellipse(600,600,300,300);
-  fill(255,209,220);
+  fill(	255, 223, 0);
   //in radians starting place is 0pi
   arc(600,600,300,300,0,(second()/30*PI));
 }
@@ -29,6 +25,31 @@ function minutes(){
   //entire canvas is filled depending on the minutes passed
   factor = h/60;
   noStroke();
-  fill(209, 220, 255);
-  rect(0, 0, w, minute()*factor);
+  fill(	0);
+  filled = minute()*factor
+  rect(0, 0, w, filled);
+  
+  return filled;
 }
+
+function hours(){
+  for (let i = 0; i < hour(); i++){
+    spawnCloud();
+  }
+}
+
+function spawnCloud(){
+  translate(500,500);
+    noStroke();
+    fill(255);
+    for (let i = 0; i < 6; i ++) {
+      ellipse(0, 0, 80, 50);
+      rotate(PI/3);
+    } 
+  
+}
+
+
+
+
+
